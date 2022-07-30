08879a13e350caf89e29274ce45ce33bf33ab138
Oleg Kaminskyy <olegkaminskyy@gmail.com>
2022-07-30 11:41:10 +0300

    task 2

--- js/script.js
+++ js/script.js
@@ -1,15 +1,56 @@
-const root = document.querySelector('#root')
-console.log(root)
+const banks = [
+  {
+    id: '435tr34wrt',
+    name: 'Mono',
+    interestRate: 5,
+    maxLoan: 500000,
+    minPayment: 1000,
+    loanTerm: 12,
+  },
+  {
+    id: 'asdfw342rew5',
+    name: 'Privat',
+    interestRate: 7,
+    maxLoan: 1000000,
+    minPayment: 5000,
+    loanTerm: 50,
+  },
+];
 
-const banksList = document.createElement('div')
-banksList.classList.add('fields__item')
-banksList.classList.add('bank__list')
-console.log(banksList)
+const root = document.querySelector('#root');
 
-const banksInformation = document.createElement('div')
-banksInformation.classList.add('fields__item')
-banksInformation.classList.add('feature__list')
-console.log(banksInformation)
+const banksList = document.createElement('div');
+banksList.classList.add('fields__item');
+banksList.classList.add('bank__list');
 
-root.append(banksList, banksInformation)
-console.log(root)
+const bankBtn = document.createElement('button');
+bankBtn.classList.add('button');
+bankBtn.textContent = 'Create new bank';
+
+const bankListUl = document.createElement('ul');
+bankListUl.classList.add('banks');
+
+banksList.append(bankListUl, bankBtn);
+
+const banksInformation = document.createElement('div');
+banksInformation.classList.add('fields__item');
+banksInformation.classList.add('feature__list');
+
+root.append(banksList, banksInformation);
+
+function createBankList(banks) {
+  bankListUl.insertAdjacentHTML(
+    'afterbegin',
+    banks
+      .map((bank, index) => {
+        return `<li class="bank__item">
+        <p>${index + 1}. ${bank.name}</p>
+        <button type="button" class="button--edit">Edit</button>
+        <button type="button" class="button--delete">Delete</button>
+      </li>`;
+      })
+      .join('')
+  );
+}
+
+createBankList(banks);
